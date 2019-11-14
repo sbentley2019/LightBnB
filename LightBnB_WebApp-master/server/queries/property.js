bde39324346068163605426bcdc4663de3ee8cc0
sbentley2019 <sbentley000@gmail.com>
2019-11-14 22:54:08 +0000

    Remove extra console.logs

--- LightBnB_WebApp-master/server/queries/property.js
+++ LightBnB_WebApp-master/server/queries/property.js
@@ -43,7 +43,6 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};`; 
 
-  console.log(queryString, queryParams);
   return db.query(queryString, queryParams)
   .then(res => res.rows);
 }
